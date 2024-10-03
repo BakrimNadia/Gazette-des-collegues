25fab9682df84eb1efd71a4c399441d2149a60de
BakrimNadia <nadia_lina@hotmail.com>
2024-10-03 16:53:53 +0200

    add component CardHome

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -2,10 +2,11 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Accueil', href: '#', current: true },
+  { name: 'Notes / Informations', href: '#', current: false },
+  { name: 'Articles', href: '#', current: false },
+  { name: 'Petites annonces', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
 ]
 
 function classNames(...classes) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+'use client';
 
+import CardHome from "./Components/cardHome";
 
 export default function Home() {
   return (
     <div>
    <h1>Bienvenue dans la gazette des collègues</h1>
+
+<div>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+    Velit voluptas ad, natus labore alias iure ea unde incidunt, 
+    quas odit quos non molestias veritatis recusandae, 
+    quibusdam enim saepe dolorem veniam.</p>
+</div>
+
+<section>
+<CardHome />
+</section>
+<section>
+
+</section>
     </div>
+
   );
 }
