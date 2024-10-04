2679770e2ff14c5ca145d6a2e80646038cd9f686
BakrimNadia <nadia_lina@hotmail.com>
2024-10-04 12:09:49 +0200

    add card and style to the home page

--- app/Components/CardHome.tsx
+++ app/Components/CardHome.tsx
@@ -5,39 +5,39 @@ import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
 export default function CardHome() {
   const list = [
     {
-      title: "Orange",
-      img: "/images/fruit-1.jpeg",
-      price: "$5.50",
+      title: "Consultez les notes d'informations",
+      img: "images/building2.jpeg",
+      subtitle: "Accédez",
     },
     {
-      title: "Tangerine",
-      img: "/images/fruit-2.jpeg",
-      price: "$3.00",
+      title: "Lisez nos articles",
+      img: "/images/healthy.jpeg",
+      subtitle: "Accédez",
     },
     {
-      title: "Raspberry",
-      img: "/images/fruit-3.jpeg",
-      price: "$10.00",
+      title: "Nos petites annonces entre collègues",
+      img: "images/machine-a-laver.jpeg",
+      subtitle: "Accédez",
     },
   ];
 
   return (
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
+    <div className="gap-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
       {list.map((item, index) => (
         <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
           <CardBody className="overflow-visible p-0">
-            <Image
+          <Image
               shadow="sm"
               radius="lg"
               width="100%"
               alt={item.title}
-              className="w-full object-cover h-[140px]"
+              className="w-full object-cover h-[350px] transition-transform duration-500 ease-in-out transform group-hover:scale-110 hover:scale-105"
               src={item.img}
             />
           </CardBody>
           <CardFooter className="text-small justify-between">
             <b>{item.title}</b>
-            <p className="text-default-500">{item.price}</p>
+            <p className="text-default-500">{item.subtitle}</p>
           </CardFooter>
         </Card>
       ))}
