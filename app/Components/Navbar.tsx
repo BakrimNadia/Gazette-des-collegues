bb1ea49fab6a18001c4ba72cd05b0c3e5f68c0ff
BakrimNadia <nadia_lina@hotmail.com>
2024-10-07 11:37:05 +0200

    add style to contact page and modify config

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Accueil', href: '#', current: true },
+  { name: 'Accueil', href: '/', current: true },
   { name: 'Notes / Informations', href: '/Informations', current: false },
   { name: 'Articles', href: '/Articles', current: false },
   { name: 'Petites annonces', href: '/Annonces', current: false },
