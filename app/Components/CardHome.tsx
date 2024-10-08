fe4d29c2ee71af71be5a4a9c453a6c026ac6165f
BakrimNadia <nadia_lina@hotmail.com>
2024-10-08 10:57:42 +0200

    add section and adjust css to the home page

--- app/Components/CardHome.tsx
+++ app/Components/CardHome.tsx
@@ -8,38 +8,49 @@ export default function CardHome() {
       title: "Consultez les notes d'informations",
       img: "images/building2.jpeg",
       subtitle: "Accédez",
+      link: "/Informations", // Lien ajouté
     },
     {
-      title: "Lisez nos articles",
+      title: "Lisez nos articles d'actualité",
       img: "/images/healthy.jpeg",
       subtitle: "Accédez",
+      link: "/Articles", 
     },
     {
-      title: "Nos petites annonces entre collègues",
-      img: "images/machine-a-laver.jpeg",
+      title: "Petites annonces entre collègues",
+      img: "images/velo.jpeg",
       subtitle: "Accédez",
+      link: "/Annonces", 
+    },
+    {
+      title: "Une suggestion ? Contactez-nous",
+      img: "images/enveloppe2.jpeg",
+      subtitle: "Accédez",
+      link: "/Contact", 
     },
   ];
 
   return (
-    <div className="gap-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+    <div className="gap-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 justify-center items-center mx-auto max-w-7xl px-4">
       {list.map((item, index) => (
-        <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
-          <CardBody className="overflow-visible p-0">
-          <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
-              alt={item.title}
-              className="w-full object-cover h-[350px] transition-transform duration-500 ease-in-out transform group-hover:scale-110 hover:scale-105"
-              src={item.img}
-            />
-          </CardBody>
-          <CardFooter className="text-small justify-between">
-            <b>{item.title}</b>
-            <p className="text-default-500">{item.subtitle}</p>
-          </CardFooter>
-        </Card>
+        <a href={item.link} key={index} className="w-full flex justify-center"> 
+          <Card shadow="sm" isPressable onPress={() => console.log("item pressed")} className="max-w-sm">
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={item.title}
+                className="w-full object-cover h-[350px] transition-transform duration-500 ease-in-out transform group-hover:scale-110 hover:scale-105"
+                src={item.img}
+              />
+            </CardBody>
+            <CardFooter className="text-small justify-between">
+              <b>{item.title}</b>
+              <p className="text-default-500">{item.subtitle}</p>
+            </CardFooter>
+          </Card>
+        </a>
       ))}
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Avatar } from "@nextui-org/react";
 import CardHome from "./Components/CardHome";
 import Image from 'next/image';
 
@@ -22,8 +23,27 @@ export default function Home() {
           fill
         />
       </section>
-      <section className="mt-8 flex justify-center items-center">
+      <section className="mt-8 justify-center items-center">
         <CardHome />
+      </section>
+      <section className="mt-8 justify-center items-center">
+        <div className="mb-4">
+        <h2 className="text-3xl font-bold tracking-tight text-center text-gray-900 sm:text-4xl">Notre équipe de rédacteurs bénévoles</h2>
+        <p className="mt-2 text-lg text-center font-bold leading-8 text-gray-600">
+          Nos collègues rédacteurs se relaient pour vous proposer des articles de qualité et 
+          vous partager les informations utiles de l&apos;entreprise, le tout dans un esprit de partage et de convivialité.
+          De plus, nous avons mis en place un système de petites annonces pour vous permettre de vendre ou d&apos;acheter des objets entre collègues, et même echanger ou proposer des services.
+          Pour partager une annonces, il suffit de nous contacter via le formulaire de contact. <br />
+        </p>
+        </div>
+      <div className="flex gap-4 justify-center items-center mb-8">
+      <Avatar isBordered src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+      <Avatar isBordered src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+      <Avatar isBordered src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+      <Avatar isBordered src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+      <Avatar isBordered src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+      <Avatar isBordered src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+    </div>
       </section>
     </div>
   );
