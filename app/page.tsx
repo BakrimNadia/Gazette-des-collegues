132a9578161cb8f1f60322b0fabe9512c254869b
BakrimNadia <nadia_lina@hotmail.com>
2024-10-16 12:05:45 +0200

    WIP adjust margin to home page

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           fill
         />
       </section>
-      <section className="mt-20 justify-center items-center">
+      <section className="mt-10 justify-center items-center">
       <div>
         <h2 className="text-3xl font-bold tracking-tight text-center text-gray-900 sm:text-4xl mb-8">
          Parcourez nos différentes rubriques
