94e59416e5b18b32e5b15719b7a891e6e20f665b
BakrimNadia <nadia_lina@hotmail.com>
2024-11-15 16:51:46 +0100

    WIP edit information page

--- app/Edit-annonce/page.tsx
+++ app/Edit-annonce/page.tsx
@@ -119,7 +119,7 @@ export default function EditAnnonce() {
         <div className="mt-10">
           <button
             type="submit"
-            className="block w-full rounded-md bg-gradient-to-r from-[#2F4F4F] to-[#00008B] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-full rounded-md bg-gradient-to-r from-[#D4AF37] to-[#A9A9A9] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Envoyer
           </button>
--- app/Edit-information/page.tsx
+++ app/Edit-information/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useAppDispatch, useAppSelector } from '../../lib/hooks';
+import { useAppDispatch, useAppSelector} from '../../lib/hooks';
 import { useState } from 'react';
 import { actionSetNews } from '../../lib/actions/news.action';
 import { actionThunkAddNews } from '../../lib/thunks/news.thunk';
@@ -12,8 +12,7 @@ export default function EditInformation() {
 
   const dispatch = useAppDispatch();
 
-  const user = useAppSelector((state) => state.user.user);
-  const newsAuthor = useAppSelector((state) => state.news.newsAuthor);
+  
 
   const [picture, setPicture] = useState('');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -22,11 +21,11 @@ export default function EditInformation() {
   const [subtitle, setSubtitle] = useState('');
   const [content, setContent] = useState('');
   const [datePublication, setdatePublication] = useState('');
-  const [userId, setUserId] = useState('');
-  
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   
 
-
+  const user = useAppSelector((state) => state.user.user);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0] || null;
@@ -65,10 +64,7 @@ export default function EditInformation() {
           dispatch(actionSetNews({ name: 'subtitle', value: subtitle }));
           dispatch(actionSetNews({ name: 'content', value: content }));
           dispatch(actionSetNews({ name: 'date_publication', value: datePublication }));
-          dispatch(actionSetNews({
-            name: 'user_id',
-            value: user ? `${newsAuthor.firstname} ${newsAuthor.lastname}` : ''
-          }));
+          dispatch(actionSetNews({ name: 'user_id', value: `${user.firstname} ${user.lastname}` }));
 
           await dispatch(actionThunkAddNews());
         }}
@@ -132,23 +128,45 @@ export default function EditInformation() {
             </div>
           </div>
 
-          <div className="sm:col-span-2">
-            <label htmlFor="author" className="block text-sm font-semibold leading-6 text-gray-900">
-              Auteur
-            </label>
-            <div className="mt-2.5">
-              <input
-                readOnly
-                id="author"
-                name="author"
-                type="text"
-                autoComplete="author"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                value={userId}
+          <div className="sm:col-span-2 grid grid-cols-2 gap-4">
+  <div>
+    <label htmlFor="firstName" className="block text-sm font-semibold leading-6 text-gray-900">
+      Prénom de l&apos;auteur
+    </label>
+    <div className="mt-2.5">
+      <input
+        required
+        id="firstName"
+        name="firstName"
+        type="text"
+        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        value={firstName}
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+      />
+    </div>
+  </div>
 
-                />
-            </div>
-          </div>
+  <div>
+    <label htmlFor="lastName" className="block text-sm font-semibold leading-6 text-gray-900">
+      Nom de l&apos;auteur
+    </label>
+    <div className="mt-2.5">
+      <input
+        required
+        id="lastName"
+        name="lastName"
+        type="text"
+        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        value={lastName}
+        onChange={(e) => {
+          setLastName(e.target.value);
+        }}
+      />
+    </div>
+  </div>
+</div>
 
           <div className="sm:col-span-2">
             <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -159,7 +177,7 @@ export default function EditInformation() {
                 required
                 id="datePiblication"
                 name="datePublication"
-                type="datePublication"
+                type="date"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={datePublication}
                 onChange={(e) => {
@@ -192,7 +210,7 @@ export default function EditInformation() {
         <div className="mt-10">
           <button
             type="submit"
-            className="block w-full rounded-md bg-gradient-to-r from-[#2F4F4F] to-[#00008B] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-full rounded-md bg-gradient-to-r from-[#D4AF37] to-[#A9A9A9] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Envoyer
           </button>
