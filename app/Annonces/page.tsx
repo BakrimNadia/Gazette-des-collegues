470e500d2d75c1b1d4be0e4b99fa52a007a0c619
BakrimNadia <nadia_lina@hotmail.com>
2024-12-13 12:51:38 +0100

    add filter to category announcement

--- app/Annonces/page.tsx
+++ app/Annonces/page.tsx
@@ -3,19 +3,24 @@
 import CardAnnonce from "../Components/CardAnnonce";
 import { IAnnouncement } from "@/@types/announcement";
 import { actionThunkAnnouncementList } from "@/lib/thunks/announcement.thunk";
+import { actionThunkCategoryList } from "@/lib/thunks/category.thunk";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useEffect, useState } from "react";
 import Loader from "../Components/Loader";
-import { Input } from "@nextui-org/react";
+import { Input, Select, SelectItem } from "@nextui-org/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 
 export default function Annonces() {
   const dispatch = useAppDispatch();
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const categories: { id: number; name: string }[] = useAppSelector((state) => state.category.categoryList);
 
   useEffect(() => {
     console.log("Dispatching action to fetch announcement");
     dispatch(actionThunkAnnouncementList());
+    dispatch(actionThunkCategoryList());
   }, [dispatch]);
 
   const announcement: IAnnouncement[] = useAppSelector((state) => state.announcement.announcementList);
@@ -27,9 +32,11 @@ export default function Annonces() {
     return <Loader />;
   }
 
-  const filteredAnnouncements = announcement.filter((announcementItem) =>
-    announcementItem.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredAnnouncements = announcement.filter((announcementItem) => {
+    const matchesSearchTerm = announcementItem.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchesCategory = selectedCategory ? announcementItem.category?.name === selectedCategory : true;
+    return matchesSearchTerm && matchesCategory;
+  });
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen mx-2">
@@ -75,38 +82,35 @@ export default function Annonces() {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <Input
-        label="Filtrez par catégorie"
-        isClearable
-        radius="lg"
-        classNames={{
-          label: "font-bold text-black/50 dark:text-white/90",
-          input: [
-            "bg-transparent",
-            "text-black/90 dark:text-white/90",
-            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-          ],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "shadow-xl",
-            "bg-default-200/50",
-            "dark:bg-default/60",
-            "backdrop-blur-xl",
-            "backdrop-saturate-200",
-            "hover:bg-default-200/70",
-            "dark:hover:bg-default/70",
-            "group-data-[focus=true]:bg-default-200/50",
-            "dark:group-data-[focus=true]:bg-default/60",
-            "!cursor-text",
-          ],
-        }}
-        placeholder="saisissez une catégorie..."
-        startContent={
-          <MagnifyingGlassIcon
-          className="h-5 w-5 text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
-        }
-      />
-    </div>
+      <Select
+          label="Filtrez par catégorie"
+          radius="lg"
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)} 
+          classNames={{
+            label: "font-bold text-black/50 dark:text-white/90",
+            popoverContent: "bg-default-200/50 dark:bg-default/60 backdrop-blur-xl shadow-xl",
+            //item: "text-black/90 dark:text-white/90 hover:bg-default-300/50",
+            trigger: [
+              "shadow-xl",
+              "bg-default-200/50",
+              "dark:bg-default/60",
+              "backdrop-blur-xl",
+              "hover:bg-default-200/70",
+              "dark:hover:bg-default/70",
+              "!cursor-pointer",
+            ],
+          }}
+          placeholder="Sélectionnez une catégorie"
+        >
+           {categories.map((category) => (
+            <SelectItem key={category.id} value={`${category.name}`}>
+              {category.name}
+        </SelectItem>
+      ))}
+           
+        </Select>
+      </div>
       <section className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-center items-center mx-2">
       {filteredAnnouncements.map((announcementItem) => {
         return <CardAnnonce key={announcementItem.id} announcementItem={announcementItem} />;
